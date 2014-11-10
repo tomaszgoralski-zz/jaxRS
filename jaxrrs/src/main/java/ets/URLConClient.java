86345ee362b183a81c051dd92ab52574ab0cb219
Tomasz Goralski <tomasz.goralski.external@atos.net>
2014-11-10 17:10:30 +0100

    ds

--- jaxrrs/src/main/java/ets/URLConClient.java
+++ jaxrrs/src/main/java/ets/URLConClient.java
@@ -4,16 +4,24 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import com.sun.research.ws.wadl.HTTPMethods;
+
 public class URLConClient {
 
 	public static void main(String[] args) throws IOException {
 		URL restURL = new URL("http://localhost:8080/SimpleRS/jax/rs/add/5/6");
-		HttpURLConnection connection = (HttpURLConnection) restURL.openConnection();
+		HttpURLConnection con = (HttpURLConnection) restURL.openConnection();
 
-		connection.setReadTimeout(100);
+		con.setReadTimeout(100);
+		con.setAllowUserInteraction(true);
+		con.setDoInput(true);
+		con.setDoOutput(true);
+		con.setRequestMethod(HTTPMethods.GET.toString());
+		con.setUseCaches(true);
+		
+		
+		// readFrom(con);
 
-		// readFrom(connection);
-
-		connection.disconnect();
+		con.disconnect();
 	}
 }
